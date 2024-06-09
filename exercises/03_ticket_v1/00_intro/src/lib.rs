7bb3dadac9bbe5d7e965cc831d65cde239535bc7
ha ha <hahyemin@haui-noteubug.local>
2024-06-09 18:48:45 +0900

    git test5

--- exercises/03_ticket_v1/00_intro/src/lib.rs
+++ exercises/03_ticket_v1/00_intro/src/lib.rs
@@ -5,11 +5,15 @@ fn intro() -> &'static str {
 =======
     "I'm ready to start modelling a software ticket!
 <<<<<<< HEAD
+<<<<<<< HEAD
     git test3"
 >>>>>>> 28aae8d (git test3)
 =======
     git test4"
 >>>>>>> 349fe73 (test4)
+=======
+    git test5"
+>>>>>>> 0f69a6a (git test5)
 }
 
 #[cfg(test)]
