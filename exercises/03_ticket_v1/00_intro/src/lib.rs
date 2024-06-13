551888a314688324529eba34b36b0cc77c3e1d2c
aehyemin <hsy13467926@gmail.com>
2024-06-13 16:55:41 +0900

    fixing conflicts

--- exercises/03_ticket_v1/00_intro/src/lib.rs
+++ exercises/03_ticket_v1/00_intro/src/lib.rs
@@ -1,23 +1,6 @@
 fn intro() -> &'static str {
     // TODO: fix me 👇
-<<<<<<< HEAD
-<<<<<<< HEAD
     "I'm ready to start modelling a software ticket!"
-=======
-    "I'm ready to start modelling a software ticket!
-<<<<<<< HEAD
-<<<<<<< HEAD
-    git test3"
->>>>>>> 28aae8d (git test3)
-=======
-    git test4"
->>>>>>> 349fe73 (test4)
-=======
-    git test5"
->>>>>>> 0f69a6a (git test5)
-=======
-    "I'm ready to start modelling a software ticket!"
->>>>>>> 7e38abd (3-3 modules)
 }
 
 #[cfg(test)]
